e600fbdc99144f2beb81961e724153ca299682be
Admilson Carneiro Cruz Filho <131927285+ryshawn@users.noreply.github.com>
2026-02-26 19:51:57 -0400

    commit2

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -49,6 +49,7 @@ in
   hardware.cpu.intel.updateMicrocode = true;
   hardware.graphics.enable = true;
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
   virtualisation.docker.enable = true; 
 
   users.users.admilson = {
@@ -60,17 +61,36 @@ in
 
   programs.firefox.enable = true;
 
+  services.printing.enable = true;
   environment.systemPackages = with pkgs; [
     unstable.vscode
     github-desktop
     fuzzel
     alacritty
     git
+    nautilus
+    loupe
+    graphite-cursors
   ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
+  xdg.mime.enable = true;
+  xdg.mime.defaultApplications = {
+    "image/jpeg" = [ "org.gnome.Loupe.desktop" ];
+    "image/png" = [ "org.gnome.Loupe.desktop" ];
+    "image/gif" = [ "org.gnome.Loupe.desktop" ];
+    "image/webp" = [ "org.gnome.Loupe.desktop" ];
+    "image/bmp" = [ "org.gnome.Loupe.desktop" ];
+    "image/tiff" = [ "org.gnome.Loupe.desktop" ];
+    "image/avif" = [ "org.gnome.Loupe.desktop" ];
+    "image/heif" = [ "org.gnome.Loupe.desktop" ];
+    "image/heic" = [ "org.gnome.Loupe.desktop" ];
+    "image/svg+xml" = [ "org.gnome.Loupe.desktop" ];
+  };
+
   programs.niri.enable = true;
+  programs.niri.useNautilus = true;
   programs.dms-shell = {
     enable = true;
     package = unstable.dms-shell;
